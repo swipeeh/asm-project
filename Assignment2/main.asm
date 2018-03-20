92bc54e94de010a8b00a89f7d6bd8f3fd5db25d6
Patrik I <260010@via.dk>
2018-03-20 13:27:20 +0100

    comments to code

--- Assignment2/main.asm
+++ Assignment2/main.asm
@@ -5,26 +5,28 @@
 ; Author : Patrik
 ;
 
+SETUP:
 
-ldi r16, 0xff           
-ldi r17, 0x00
-out ddra, r16           
-out ddrb, r17
-/*
-IN r18, pinb
-OUT porta, r18*/
+	ldi		r16, 0xff				;inserting 1111_1111 value into r16       
+	ldi		r17, 0x00				;inserting 1111_1111 value into r17
+	out		ddra, r16				; configure pins from portA  
+	out		ddrb, r17				;configrue pins from portB
 
-ldi r18, 0b11111110
-ldi r21, 0b11111011
-ldi r20, 0x00
+	ldi		r18, 0b11111110
+	ldi		r21, 0b11111011
+	ldi		r20, 0x00
+
+	/*
+	IN r18, pinb
+	OUT porta, r18*/
 
 start:
-	out porta, r18;
+	out		porta, r18;
 
 
-	in r19, pinb
+	in		r19, pinb
 
-	cp r18, r19
-	brne start
-	out porta, r20
-	rjmp start
+	cp		r18, r19
+	brne	start
+	out		porta, r20
+	rjmp	start				;jump to start of the loop
