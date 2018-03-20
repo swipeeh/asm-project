7578bcf886885dc2765dc8f6dfa4a7cacc6b87b9
Patrik I <260010@via.dk>
2018-03-20 11:06:58 +0100

    basic tex file paih branch

--- basics.asm
+++ basics.asm
@@ -25,3 +25,7 @@ LDI    R16,  0xFF       ; Load 0b11111111 in R16
  
                  LDI    R16,  0x00       ; Load 0b00000000 in R16
                  OUT    DDRB, R16        ; Configure PortB as an Input port
+				 
+				 
+				 MROCHTA
+				 
