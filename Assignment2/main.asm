caae0e7fa9d1fd4faa238fc42c1066256793bfe3
Zahari Dzhelepov <253925@via.dk>
2018-03-26 13:06:08 +0200

    added clearDisplay,a delay and indicator if level won or lost

--- Assignment2/main.asm
+++ Assignment2/main.asm
@@ -16,6 +16,12 @@ SETUP:
 	ldi		r21, 0b11111011
 	ldi		r20, 0x00
 
+;stack setup
+	ldi		r16, high(RAMEND)
+	out		sph, r16
+	ldi		r16, low(RAMEND)
+	out		spl, r16
+	
 	/*
 	IN r18, pinb
 	OUT porta, r18*/
@@ -30,3 +36,37 @@ start:
 	brne	start
 	out		porta, r20
 	rjmp	start				;jump to start of the loop
+
+clearDisplay:			;clear display after a level/userInput/start or end game
+	push	r17
+	ldi		r17, 0b11111111
+	out		porta, r17
+	pop		r17
+	ret
+
+
+delay:		;delay function (for int i=0; i > 0; i--)
+	nop
+	push	r26
+	push	r27
+	ldi		r27,255
+loop1:
+	ldi		r26,255
+	dec		r27
+	brne	end
+loop2:
+	dec		r26
+	brne	loop1
+end:
+	pop		r27
+	pop		r26
+	ret
+
+
+levelWon:
+	ldi		r16, 0b00000000		;all lights on
+	out		porta, r16			;show on the board that all lights are on
+
+levelLost:
+	ldi		r17, 0b01010101
+	out		porta, r17
